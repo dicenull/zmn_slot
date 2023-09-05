3d8f2f1538a6eedd4a03d505acf651de30fccb18
dicenull <dicenull@users.noreply.github.com>
2023-09-05 14:19:00 +0000

    deploy: f3a519ca6b1322cb0214b5fa2bfb10d8681e7f1f

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:app/app.dart';
-import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(const ProviderScope(child: App()));
-}
